2a13412d4b01dcdba11461bde75b2fbc566d78a0
JohnChristidis <auto45056@uniwa.gr>
2024-01-22 11:47:27 +0200

    22_1
    Added more gemTopics

--- src/utils/gemTopics.js
+++ src/utils/gemTopics.js
@@ -211,7 +211,226 @@ const gemTopicsData = {
               },
             ]
           }
-        },        
+        },
+        {
+          "gemTopic": {
+            "id" : 5,
+            "src" : "F",
+            "header": "Project Leader",
+            "desc": "\"You are part of a group for a class project. However two the members, Tom and Dennis, start arguing about who should be the leader of the project.\"",
+            "img" : "",
+            "question" : "What are you doing?",
+            "choices" : [
+              {
+                "choice" : {
+                  "id" : 0,
+                  "text" : "I support Tom. He is the best student in the class and he will surely get us a good grade.",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 1,
+                  "text" : "I support Dennis. He is the most popular boy in the class and you like him very much.",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 2,
+                  "text" : "I decide to say that we can take important decisions together.",
+                  "answer" : true
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 3,
+                  "text" : "I suggest myself as a leader. I am certainly the most capable one.",
+                  "answer" : false
+                }
+              },
+            ]
+          }
+        },
+        {
+          "gemTopic": {
+            "id" : 6,
+            "src" : "G",
+            "header": "Bulling a classmate",
+            "desc": "\"You are observing a group of children as they yell at a classmate. They throw his books on the floor and start to pushing him. Around the group, many other observers watch the scene without doing anything.\"",
+            "img" : "",
+            "question" : "Choose the ending of the story",
+            "choices" : [
+              {
+                "choice" : {
+                  "id" : 0,
+                  "text" : "I keep watching what happens, doing nothing. I'm too scared.",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 1,
+                  "text" : "I start laughing. After all, they're just kidding.",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 2,
+                  "text" : "The pushed child is also very scared. I decide to run and call an adult to stop what is happening.",
+                  "answer" : true
+                }
+              },
+            ]
+          }
+        },
+        {
+          "gemTopic": {
+            "id" : 7,
+            "src" : "H",
+            "header": "Traditional food",
+            "desc": "\"Shui, your classmate who recently moved from Beijing, is eating  jiaozi, a typical chinese dish. However, other classmates start making nasty comments about the smell of the child's food.\"",
+            "img" : "",
+            "question" : "In your opinion, what is Shui thinking?",
+            "choices" : [
+              {
+                "choice" : {
+                  "id" : 0,
+                  "text" : "He doesn't have a sad face. He probably isn't interested in what other people are saying.",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 1,
+                  "text" : "Although Shui does not seem upset or sad, he is probably offended by what others are saying.",
+                  "answer" : true
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 2,
+                  "text" : "He's just thinking about eating his snack.",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 3,
+                  "text" : "I can not answer",
+                  "answer" : false
+                }
+              },
+            ]
+          }
+        },
+        {
+          "gemTopic": {
+            "id" : 8,
+            "src" : "I",
+            "header": "Eating alone",
+            "desc": "A little girl with a sad expression is sitting on the sidelines in the cafeteria eating by herself. On the other side of the room is the table where your classmates have happy expressions as they are eating together.",
+            "img" : "",
+            "question" : "Describe accurately what is happening",
+            "choices" : [
+              {
+                "choice" : {
+                  "id" : 0,
+                  "text" : "We are in a school cafeteria and everyone is having a lunch.",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 1,
+                  "text" : "A little girl is eating on the sidelines while there is a table with many children who happily eat together",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 2,
+                  "text" : "A little girl eats on the sidelines and looks sad. She would probably like to join the other guys' table.",
+                  "answer" : true
+                }
+              },
+            ]
+          }
+        },
+        {
+          "gemTopic": {
+            "id" : 9,
+            "src" : "J",
+            "header": "Islamic Holiday",
+            "desc": "During recreation, one of your classmates with Tunisian origins explains to you that today is an important Islamic holiday, called 'Eid Al-Adha'. She explains that she is a little sad because she would like to share this holiday with all the other classmates but she is a afraid of their reaction.",
+            "img" : "",
+            "question" : "What are you doing?",
+            "choices" : [
+              {
+                "choice" : {
+                  "id" : 0,
+                  "text" : "I try to cheer her up by trying to entertain her",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 1,
+                  "text" : "I tell her not to think about it too much.",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 2,
+                  "text" : "I tell her that it's good not to talk about it with others because they would probably be bored",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 3,
+                  "text" : "I ask the teacher to discuss with the class about the Islamic holiday your friend told you about.",
+                  "answer" : true
+                }
+              },
+            ]
+          }
+        },
+        {
+          "gemTopic": {
+            "id" : 10,
+            "src" : "K",
+            "header": "Where do you go",
+            "desc": "A child is sitting alone with a sad expression. Further away there is a group of 5 children sitting near a ball. Still a little further away there is a group of 3 children talking happily to each other.",
+            "img" : "",
+            "question" : "Where do you decide to go?",
+            "choices" : [
+              {
+                "choice" : {
+                  "id" : 0,
+                  "text" : "I’ll sit next the lonely child.",
+                  "answer" : true
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 1,
+                  "text" : "I’ll go near the group of 5 children with the ball to play with them",
+                  "answer" : false
+                }
+              },
+              {
+                "choice" : {
+                  "id" : 2,
+                  "text" : "I’ll go near the group of 3 children to chat with them",
+                  "answer" : false
+                }
+              },
+            ]
+          }
+        },
 
 
     ]
